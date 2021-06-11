199fae77afd7d0f55c640ea81dc10c31ea549fe1
nsurendran1991 <nsurendran@atlassian.com>
2021-06-11 14:32:29 +1000

    first commit

---
+++ src/test/java/com/example/demo/SampleApplicationTests.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void demoTest() {
+		assert(true);
+	}
+
+	@Test
+	void demoTest2() {
+		assert(true);
+	}
+
+}
